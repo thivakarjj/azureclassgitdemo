7e40e3830864a9180d36d11f08d1ee31ede5f5a4
Thivakar <thivakar21@gmail.com>
2024-05-28 18:18:52 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,85 @@
+locals {
+  resourceGroupName  = "${var.prefix}-${var.resourceFunction}-${var.environment}-${var.region}"
+  storageAccountName = "${var.prefix}${var.resourceFunction}sa${var.environment}${var.region}"
+  apimName          = "${var.prefix}-${var.resourceFunction}-${var.environment}-${var.region}"
+  appInsightsName   = "${var.prefix}-${var.resourceFunction}-appinsights-${var.environment}-${var.region}"
+}
+
+# create resource group
+resource "azurerm_resource_group" "rg" {
+  name     = local.resourceGroupName
+  location = var.location
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                      = local.storageAccountName
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_replication_type = var.replication
+  account_tier             = var.account_tier
+  account_kind              = var.account_kind
+  enable_https_traffic_only = true
+}
+
+#Create storage account container for apim
+resource "azurerm_storage_container" "saContainerApim" {
+  name                  = var.apim-files
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = var.container_access_type
+}
+
+# Create storage account container for api
+resource "azurerm_storage_container" "saContainerApi" {
+  name                  = var.api-file
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = var.container_access_type
+}
+
+# Create Azure API management resource
+
+resource "azurerm_api_management" "apim" {
+  name                = local.apimName
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = var.apimPublisherName
+  publisher_email     = var.apimPublisherEmail
+  sku_name            = var.apimSku
+  identity {
+    type = "SystemAssigned"
+  }
+}
+# Create Application Insights
+resource "azurerm_application_insights" "ai" {
+  name                = local.appInsightsName
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = var.application_type
+
+}
+# Create Logger
+resource "azurerm_api_management_logger" "apimLogger" {
+  name                = "${local.apimName}-logger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name  = azurerm_resource_group.rg.name
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.ai.instrumentation_key
+  }
+}
+
+# Create API
+resource "azurerm_api_management_api" "api" {
+  name                = var.apiname
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = var.apidisplayname
+  path                = var.apipath
+  protocols           = ["https"]
+
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
+  }
+}
